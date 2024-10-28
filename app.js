e759ebf26382be335652449a196b83f0dffef416
Shiv Patel <shivpatel0204@gmail.com>
2024-10-27 22:54:44 -0400

    .

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const canvas = document.getElementById('myCanvas'); // Get the canvas element
 const ctx = canvas.getContext('2d'); // Get the 2D drawing context
 
 
-let isDrawing = true;
+let drawing = false;
 let startX, startY;
 let currentShape = 'line';
-let selectedColor = '#000000';
 
 // Adds event listeners for mouse events to handle the controls drawing.
 canvas.addEventListener('mousedown', startDrawing);
@@ -18,34 +17,21 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', stopDrawing);
 
 // drawing
-function startDrawing(c) {
-    isDrawing = true;
-    startX = c.offsetX;
-    startY = c.offsetY;
+function startDrawing(line) {
+    drawing = true;
+    startX = line.offsetX;
+    startY = line.offsetY;
     ctx.beginPath();
-    ctx.strokeStyle = selectedColor;
 }
 
-// Stop drawing
+// Drawing Stop
+//function endDrawing(line) {
+  //  if (!drawing) return;
+    //const endX = line.offsetX;
+    //const endY = line.offsetY;
+//}
+
+    // Stop drawing
 function stopDrawing() {
     isDrawing = false;
 }
-
-
-// Drawing based on selected shape
-function draw(c) {
-    if (!isDrawing) return;
-
-    const endX = c.offsetX;
-    const endY = c.offsetY;
-
-    // Clear the canvas and redraw to avoid artifacts
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    switch (currentShape) {
-        case 'line':
-            ctx.moveTo(startX, startY);
-            ctx.lineTo(endX, endY);
-            ctx.stroke();
-            break;
-    }}
